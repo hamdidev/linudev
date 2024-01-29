48749ed64de91360d2f8035b6ebe147b396f446e
hamdidev <itechtranss77@gmail.com>
2024-01-29 21:23:21 +0300

    add light/dark theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,15 @@ import Main from "./App.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <Main />
+        <ThemeProvider>
+          <Main />
+        </ThemeProvider>
       </Provider>
     </PersistGate>
   </React.StrictMode>
